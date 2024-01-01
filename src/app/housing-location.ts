3cf400599d0ea5db37eaf0cf3239d5a919a5c98c
princenyamadi <=>
2024-01-01 22:53:41 +0000

    add app ui

---
+++ src/app/housing-location.ts
@@ -0,0 +1,12 @@
+export interface HousingLocation {
+
+    id: number,
+    name: string,
+    state: string,
+    city:string,
+    photo: string,
+    availableUnits: number,
+    wifi: boolean,
+    laundry: boolean,
+
+}

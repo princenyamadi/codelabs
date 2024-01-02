91cd74e7d57126e373ef583b2ca2f5c5be42744d
princenyamadi <=>
2024-01-02 14:58:56 +0000

    change static data to promises

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housing-location';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   template: `
   <section class="listing">
     <img [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}" class="listing-photo">
@@ -13,6 +14,7 @@ import { HousingLocation } from '../housing-location';
       {{housingLocation.name}}
     </h2>
     <p class="listing-location">{{housingLocation.city}}, {{housingLocation.state}}</p>
+    <a [routerLink]="['details/',housingLocation.id]">Learn More</a>
   </section>
   `,
   styleUrls: ['./housing-location.component.css']
